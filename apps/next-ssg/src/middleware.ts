b3d28fb149b547a7b8e96883855ad3600e3d9a10
Bailig <barrybailig@hotmail.com>
2022-10-31 23:08:03 +0300

    fix domain

--- apps/next-ssg/src/middleware.ts
+++ apps/next-ssg/src/middleware.ts
@@ -2,7 +2,7 @@ import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
 export function middleware(request: NextRequest) {
-  if (!request.nextUrl.hostname.includes("b-monorepo")) {
+  if (!request.nextUrl.hostname.includes("mono.fyrlabs")) {
     return;
   }
   const whiteLabelSubdomain = request.nextUrl.hostname.split(".")[0];
