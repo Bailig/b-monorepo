d15812eca204a1836716d2c88bbdd63558d3d544
Bailig <barrybailig@hotmail.com>
2022-10-31 22:54:41 +0300

    added middleware

---
+++ apps/next-ssg/src/middleware.ts
@@ -0,0 +1,12 @@
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+
+export function middleware(request: NextRequest) {
+  if (!request.nextUrl.hostname.includes("b-monorepo")) {
+    return;
+  }
+  const whiteLabelSubdomain = request.nextUrl.hostname.split(".")[0];
+  const newUrl = request.nextUrl.clone();
+  newUrl.pathname = whiteLabelSubdomain + request.nextUrl.pathname;
+  return NextResponse.rewrite(newUrl);
+}
